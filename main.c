38474d5ea18297f436c219c071dd3daab62d7462
Larthanor <lartragnarock@gmail>
2014-05-26 09:56:49 +0200

    Main oublié trollol (>'_')> <('_'<)

--- main.c
+++ main.c
@@ -3,7 +3,20 @@
 
 int main(void)
 {
-	printf("Hello 42 ! How are you today ?\n");
+	int menu = 0;
+	do
+	{
+		printf("Bienvenu sur le menu principal de la bibliotheque. Veuillez entrer le numero du menu que vous voulez utiliser :\n0) Menu de Services \n1) Menu interface\n");
+		scanf("%d", &menu);
+	} while ((menu > 2) || (menu < -1));
+	if (!menu)
+	{
+		printf("42\n");
+	}
+	else
+	{
+		printf("LEEEEEEROOYYYYYYY JEEEEEEENKIIIIINS\n");
+	}
 	system("pause");
 
 	return EXIT_SUCCESS;
